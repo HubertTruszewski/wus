7aceb6992c123bdb76227d99415c0416a342de77
Hubert Truszewski <hubitru@gmail.com>
2023-01-07 23:39:34 +0100

    Add forgotten script

---
+++ lab2/install_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+
+sudo apt-get update
+sudo apt-get install --yes ca-certificates curl gnupg lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --no-tty --batch --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+sudo apt update
+sudo apt-get install --yes docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
+sudo groupadd docker
+sudo usermod -aG docker "$USER"
